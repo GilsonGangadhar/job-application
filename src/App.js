09866135274e56017f1fa131f54d4af0cb78cf81
GilsonGangadhar <gilsongangadhar@gmail.com>
2020-04-02 14:03:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Link} from 'react-router-dom'
+import FrontEnd from './FrontEnd'
+import Nodejs from './Nodejs'
+import MeanStack from './MeanStack'
+import FullStack from './FullStack'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props){
+    return (
+        <BrowserRouter>
+         <div>
+             <h1> Admin Dashboard</h1>
+             <Link to='/front-end'> Front-End Developer</Link>||
+             <Link to='/nodejs'> Node.js Developer</Link>||
+             <Link to='/mean-stact'> MEAN Stack Developer</Link>||
+             <Link to='/full-stack'> Full Stack Developer</Link>
+
+
+
+             <Route path='/front-end' component = {FrontEnd} />
+             <Route path='/nodejs' component = {Nodejs} />
+             <Route path='/mean-stact'  component = {MeanStack}  />
+             <Route path='/full-stack'  component = {FullStack} />
+
+             
+
+         </div>
+        </BrowserRouter>
+    )
 }
-
-export default App;
+export default App
